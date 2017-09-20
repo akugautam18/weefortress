497d7e391449165c39a0fe4acda163312b28bb0b
Anurodh Malviya <anurodh.m@GGN000391-2.local>
2017-09-20 16:11:10 +0530

    test template

--- tf2/urls.py
+++ tf2/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.index,name='index'),
     url(r'^(?P<team_id>[0-9]+)/$', views.team_detail, name='team_detail'),
     url(r'^gm_d/(?P<team_id>[0-9]+)/$', views.team_detail, name='game_detail'),
-    
+    url(r'^test/$', views.test, name='test'),
 ]
--- tf2/views.py
+++ tf2/views.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 
+from django.shortcuts import render
+from django.template import context
+from django.template.loader import get_template
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>this is my first app</h1>")
 
+
 def team_detail(request,team_id):
     return HttpResponse("Team id is %s." % team_id)
-        
+
+
+def test(request):
+    temp = get_template('test.html')
+    response = temp.render()
+    return HttpResponse(response)
